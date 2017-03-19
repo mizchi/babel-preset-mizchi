f2dbb961daec335ff7ec46f59bca0d5e6a8de899
mizchi <miz404@gmail.com>
2017-03-20 05:02:18 +0900

    Init

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "presets": [
+    "react",
+    "flow"
+  },
+  "plugins": [
+    "dynamic-import-webpack",
+    "transform-decorators-legacy",
+    "transform-class-properties",
+    "transform-runtime",
+    "transform-object-rest-spread"
+  ]
+}
