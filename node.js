16bf3b49838784f4b56c23ec42635895afd028e2
mizchi <miz404@gmail.com>
2017-05-01 04:51:15 +0900

    Add node with flow comments

--- node.js
+++ node.js
@@ -1,12 +1,7 @@
 module.exports = {
-  "presets": [
-    "react",
-    "flow"
-  ],
   "plugins": [
-    "transform-decorators-legacy",
     "transform-class-properties",
-    "transform-runtime",
+    "transform-flow-comments",
     "transform-object-rest-spread"
   ]
 }
